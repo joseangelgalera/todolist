75f91388324e9a770b80fb6f6d5a0689227105d3
joseangelgalera <joseangelgg96@gmail.com>
2025-01-21 12:02:27 +0100

    Prueba

--- public/index.php
+++ public/index.php
@@ -11,7 +11,6 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
-log
 
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
